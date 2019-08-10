7aa249bd1e706731c0da2cbefb0c53ad0db8ac2b
dcardona91 <alonsoloaiza@gmail.com>
2019-08-10 18:54:35 -0500

    minor changes

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ const unifiedServer = (req, res) => {
 
 const getMessage = () => {
   const random = Math.floor(Math.random() * messages.length);
-  return {'message': messages[random].message}
+  return {'welcomeMsessage': messages[random].message}
 };
 
 // Define all the handlers
